ddbdcd161ced97b95e05f7f95395f9daca4f8077
blueyi <blueyiniu@qq.com>
2016-03-27 21:23:19 +0800

    bak

--- include/common.h
+++ include/common.h
@@ -8,7 +8,17 @@
 
 #ifndef COMMON_H
 #define COMMON_H
+
+
+
 /******Const value*****/
+const double pi = 3.14159265359;
+const double waterrhow = 1025.0;
+const int npair = 50;
+const int npair_struct_bound = 500;
+const int nGFpair = 20;
+const double maxsn = -1e9;
+const double maxst = -1e9;
 
 
 
