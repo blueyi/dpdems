adf403d4b4628d329b6c9b3e7421c83e56cac7db
blueyi <blueyiniu@qq.com>
2016-05-06 11:35:50 +0800

    c++11 support

--- cuda/make.py
+++ cuda/make.py
@@ -10,16 +10,16 @@
 import os
 import platform
 
-compile_config = os.system('nvcc -c ../src/config.cpp  --ptxas-options=-v')
-compile_inputdata = os.system('nvcc -c ../src/inputDatas.cpp  --ptxas-options=-v')
-compile_common = os.system('nvcc -c ../src/common.cpp  --ptxas-options=-v')
-compile_search = os.system('nvcc -c ./dpdems_cuda.cu  --ptxas-options=-v')
+compile_config = os.system('nvcc -c ../src/config.cpp  --ptxas-options=-v -std=c++11')
+compile_inputdata = os.system('nvcc -c ../src/inputDatas.cpp  --ptxas-options=-v -std=c++11')
+compile_common = os.system('nvcc -c ../src/common.cpp  --ptxas-options=-v -std=c++11')
+compile_search = os.system('nvcc -c ./dpdems_cuda.cu  --ptxas-options=-v -std=c++11')
 
 if compile_inputdata == 0 and compile_common == 0 and compile_search == 0 and compile_config == 0 :
   if platform.system() == 'Windows' : 
-    link = os.system('nvcc common.obj config.obj inputDatas.obj dpdems_cuda.obj -o Dpdems_cuda ')
+    link = os.system('nvcc common.obj config.obj inputDatas.obj dpdems_cuda.obj -o Dpdems_cuda -std=c++11')
   else:
-    link = os.system('nvcc common.o config.o inputDatas.o dpdems_cuda.o -o Dpdems_cuda ')
+    link = os.system('nvcc common.o config.o inputDatas.o dpdems_cuda.o -o Dpdems_cuda -std=c++11')
 if link == 0 :
   print('link success!')
   isrun = input('Run? y or n : ')
